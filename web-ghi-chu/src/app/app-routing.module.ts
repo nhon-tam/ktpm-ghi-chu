b1376996da118938bd4159de740831e0af2b3afa
nhontam <daonhontam@gmail.com>
2022-06-20 08:59:13 +0700

    Update app-routing.module.ts

--- web-ghi-chu/src/app/app-routing.module.ts
+++ web-ghi-chu/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard/note'  },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard/note'  },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) , canActivate: [AuthGuard],},
   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) , canActivate: [AuthGuard],},
   { path: 'dashboard', loadChildren: () => import('./dashboard-note/dashboard-note.module').then(m => m.DashboardNoteModule), canActivate: [AuthGuard] }
